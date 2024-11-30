bc5a9eb4060431ea26ff57070d82d8c0d62d9fa8
Kashif <admin@kashifali.me>
2024-11-30 16:17:14 +0500

    Added my scheduler

--- routes/console.php
+++ routes/console.php
@@ -10,4 +10,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command(ScrapArticles::class)->everyThirtyMinutes();
+Schedule::command(ScrapArticles::class)->everySecond();
