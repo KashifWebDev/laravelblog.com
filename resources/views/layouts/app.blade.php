ea5bba41984bcfd25a54465d0757928a5f3e8b1a
Kashif <admin@kashifali.me>
2024-11-30 23:12:47 +0500

    Added gtag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,13 @@
 
         gtag('config', 'G-3R7G5BFS1C');
     </script>
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "p6rrxtlggs");
+    </script>
 
 
     <meta charset="utf-8" />
