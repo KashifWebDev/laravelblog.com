88f50d2538f8920ff17a03ea7431769875c7bd9b
Kashif <admin@kashifali.me>
2025-01-06 14:05:38 +0500

    Sitemap.xml

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Article;
+use App\Models\Course;
 use Illuminate\Console\Command;
 use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Tags\Url;
@@ -16,8 +17,8 @@ class GenerateSitemap extends Command
     {
         $sitemap = Sitemap::create();
 
+        // Add articles to sitemap
         $articles = Article::all();
-
         foreach ($articles as $article) {
             $sitemap->add(
                 Url::create(route('article.read', $article->slug))
@@ -25,6 +26,35 @@ class GenerateSitemap extends Command
                     ->setPriority(0.8)
                     ->setChangeFrequency('weekly')
             );
+        }
+
+        // Add courses and lessons to sitemap
+        $courses = Course::all();
+        foreach ($courses as $course) {
+            // Add course page
+            $sitemap->add(
+                Url::create(route('courses.show', $course->slug))
+                    ->setLastModificationDate($course->updated_at)
+                    ->setPriority(0.9)
+                    ->setChangeFrequency('weekly')
+            );
+
+            // Ensure the course has lessons before looping
+            if ($course->lessons()->exists()) {
+                foreach ($course->lessons as $lesson) {
+
+//                    echo $course->slug. ' __ '. $lesson->slug.PHP_EOL;
+//                    echo route('courses.show.lesson', ['course' => $course->slug, 'lesson' => $lesson->slug]).PHP_EOL;
+                    if($course->slug != '' && $lesson->slug != ''){
+                        $sitemap->add(
+                            Url::create(route('courses.show.lesson', ['course' => $course->slug, 'lesson' => $lesson->slug]))
+                                ->setLastModificationDate($lesson->updated_at)
+                                ->setPriority(0.7)
+                                ->setChangeFrequency('weekly')
+                        );
+                    }
+                }
+            }
         }
 
         $sitemap->writeToFile(public_path('sitemap.xml'));
