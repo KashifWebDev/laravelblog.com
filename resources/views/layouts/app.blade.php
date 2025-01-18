94ba53e1407d396a233078ba5286fb4e7a882101
Kashif <admin@kashifali.me>
2025-01-18 15:22:03 +0500

    Clarity script added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W3DK2P4B');</script>
     <?php $desc = 'Discover the ultimate resource for Laravel and PHP development. Access in-depth tutorials, expert coding tips, and the latest tools and trends to elevate your skills. Stay ahead with insights into cutting-edge techniques and best practices for building robust, scalable web applications."
 
 This version includes strategic keywords like "ultimate resource," "in-depth tutorials," "cutting-edge techniques," and "scalable web applications" to improve search rankings while keeping the content engaging and informative'; ?>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3R7G5BFS1C"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-3R7G5BFS1C');
+    </script>
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "p6rrxtlggs");
+    </script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
